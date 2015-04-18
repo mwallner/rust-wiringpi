8968794d7f91d6bf5e325c8dee27bb675b03019c
Erik Hedvall <erikwhedvall@gmail.com>
2015-04-18 15:33:22 +0200

    Make it possible to skip building wiringPi C library

--- build.rs
+++ build.rs
@@ -2,11 +2,13 @@ use std::process::Command;
 use std::env;
 
 fn main() {
-    let out_dir = env::var("OUT_DIR").unwrap();
-    match Command::new("make").arg("-e").status() {
-        Ok(status) if !status.success() => panic!("failed to build wiringPi C library (exit code {:?})", status.code()),
-        Err(e) => panic!("failed to build wiringPi C library: {}", e),
-        _ => {}
+    if !cfg!(feature = "noclib") {
+        let out_dir = env::var("OUT_DIR").unwrap();
+        match Command::new("make").arg("-e").status() {
+            Ok(status) if !status.success() => panic!("failed to build wiringPi C library (exit code {:?})", status.code()),
+            Err(e) => panic!("failed to build wiringPi C library: {}", e),
+            _ => {}
+        }
+        println!("cargo:rustc-flags=-L native={} -l static=wiringpi", out_dir);
     }
-    println!("cargo:rustc-flags=-L native={} -l static=wiringpi", out_dir);
 }
