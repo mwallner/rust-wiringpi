259299cf87b26a37a57b4c12e959c27f13a07805
Razican <razican@protonmail.ch>
2016-05-23 18:42:16 +0200

    Removed noclib feature, not needed anymore

--- build.rs
+++ build.rs
@@ -7,13 +7,11 @@ const TARGET: &'static str = "wiringpi";
 const TARGET: &'static str = "wiringop";
 
 fn main() {
-    if !cfg!(feature = "noclib") {
-        let out_dir = env::var("OUT_DIR").unwrap();
-        match Command::new("make").arg("-e").arg(TARGET).status() {
-            Ok(status) if !status.success() => panic!("failed to build wiringPi C library (exit code {:?})", status.code()),
-            Err(e) => panic!("failed to build wiringPi C library: {}", e),
-            _ => {}
-        }
-        println!("cargo:rustc-flags=-L native={} -l static=wiringpi", out_dir);
+    let out_dir = env::var("OUT_DIR").unwrap();
+    match Command::new("make").arg("-e").arg(TARGET).status() {
+        Ok(status) if !status.success() => panic!("failed to build wiringPi C library (exit code {:?})", status.code()),
+        Err(e) => panic!("failed to build wiringPi C library: {}", e),
+        _ => {}
     }
+    println!("cargo:rustc-flags=-L native={} -l static=wiringpi", out_dir);
 }
