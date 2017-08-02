cb2982f45a40f0fd8b2a47fe1c6c456480adafca
Hirokazu Nishioka <hiro@nisshiee.org>
2017-08-02 21:40:47 +0900

    Add development mode
    New feature `development` mode.
    It replace wiringpi bindings with dummy functions which only output simple logs.
    This feature enables our projects using rust-wiringpi to be compiled on development environments.

--- build.rs
+++ build.rs
@@ -7,6 +7,8 @@ const TARGET: &'static str = "wiringpi";
 const TARGET: &'static str = "wiringop";
 
 fn main() {
+    if cfg!(feature = "development") { return; }
+
     let out_dir = env::var("OUT_DIR").unwrap();
     match Command::new("make").arg("-e").arg(TARGET).status() {
         Ok(status) if !status.success() => panic!("failed to build wiringPi C library (exit code {:?})", status.code()),
